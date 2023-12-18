193a1ef48cec95967bc691018508817c274cbfa5
Алексей Замятин <79986929+RoboutGulman@users.noreply.github.com>
2023-12-18 16:00:23 +0300

    добавил реализации методов репозиториев

--- company/impl/infrastructure/sql/employeeAccountRepository.go
+++ company/impl/infrastructure/sql/employeeAccountRepository.go
@@ -17,8 +17,15 @@ type employeeAccountRepository struct {
 }
 
 func (r employeeAccountRepository) CreateEmployee(ctx context.Context, dto domain.EmployeeRequest, userId int, companyId int) error {
-	//TODO implement me
-	panic("implement me")
+	query := `
+		INSERT INTO employeeaccount
+		(firstname, secondname, surname,
+		telephonenumber, avatarurl, dateofbirth)
+		VALUES ($1, $2, $3,	$4, $5, $6)
+	`
+	rows, err := r.conn.Query(ctx, query, dto.FirstName, dto.SecondName, dto.Surname, dto.TelephoneNumber, dto.Icon, dto.DateOfBirth)
+	defer rows.Close()
+	return err
 }
 
 func (r employeeAccountRepository) GetEmployee(ctx context.Context, id int) (domain.EmployeeWithConnections, error) {
@@ -78,26 +85,104 @@ func (r employeeAccountRepository) GetEmployee(ctx context.Context, id int) (dom
 }
 
 func (r employeeAccountRepository) GetCompanyEmployee(ctx context.Context, userId int, companyId int) (domain.EmployeeWithConnections, error) {
-	//TODO implement me
-	panic("implement me")
+	var employee domain.EmployeeWithConnections
+	var err error
+	var rows pgx.Rows
+	query := `
+		SELECT employeeaccount.id, employeeaccount.firstname,
+		       employeeaccount.secondname, employeeaccount.surname,
+		       employeeaccount.dateofbirth, employeeaccount.telephonenumber,
+		       employeeaccount.avatarurl, company.id, company.name, auth_user.email
+		FROM employeeaccount
+		LEFT JOIN company ON employeeaccount.companyid=company.id
+		LEFT JOIN auth_user ON employeeaccount.userid=auth_user.id
+        WHERE employeeaccount.id=$1 AND employeeaccount.companyid=$2
+	`
+	err = r.conn.QueryRow(ctx, query, userId, companyId).Scan(&employee.Id, &employee.FirstName,
+		&employee.SecondName, &employee.Surname,
+		&employee.DateOfBirth, &employee.TelephoneNumber,
+		&employee.Icon, &employee.Company.Id,
+		&employee.Company.Name, &employee.Email)
+	if err == pgx.ErrNoRows {
+		return employee, employeeaccount.EmployeeNotFound
+	} else if err != nil {
+		return employee, err
+	}
+	query = `
+		SELECT department.id, department.name
+		FROM department
+		RIGHT JOIN employee_department ON employee_department.departmentid=department.id
+		RIGHT JOIN employeeaccount ON employeeaccount.id=employee_department.accountid
+		WHERE employeeaccount.id=&1 AND department.companyid=$2
+	`
+	rows, err = r.conn.Query(ctx, query, userId, companyId)
+	for rows.Next() {
+		var departmentInfo domain.DepartmentInfo
+		err = rows.Scan(departmentInfo.Id, departmentInfo.Name)
+		if err != nil {
+			return employee, err
+		}
+		employee.Departments = append(employee.Departments, departmentInfo)
+	}
+	query = `
+		SELECT role.id, role.title
+		FROM role
+		RIGHT JOIN employee_roles ON employee_roles.roleid=role.id
+		RIGHT JOIN employeeaccount ON employeeaccount.id=employee_roles.accountid
+		WHERE employeeaccount.id=$1 AND employeeaccount.companyid=$2
+	`
+	rows, err = r.conn.Query(ctx, query, userId, companyId)
+	for rows.Next() {
+		var roleInfo domain.RoleInfo
+		err = rows.Scan(roleInfo.Id, roleInfo.Name)
+		if err != nil {
+			return employee, err
+		}
+		employee.Roles = append(employee.Roles, roleInfo)
+	}
+	return employee, nil
 }
 
 func (r employeeAccountRepository) DeleteEmployee(ctx context.Context, id int) error {
-	//TODO implement me
-	panic("implement me")
+	query := `
+		DELETE FROM employeeaccount
+		WHERE id=$1
+	`
+	rows, err := r.conn.Query(ctx, query, id)
+	defer rows.Close()
+	return err
 }
 
 func (r employeeAccountRepository) EditEmployee(ctx context.Context, id int, dto domain.EmployeeRequest) error {
-	//TODO implement me
-	panic("implement me")
+	query := `
+		UPDATE employeeaccount
+		SET firstname=$2, secondname=$3, surname=$4,
+		    telephonenumber=$5, avatarurl=$6, dateofbirth=$7,
+		WHERE id=$1
+	`
+	rows, err := r.conn.Query(ctx, query, id, dto.FirstName, dto.SecondName, dto.Surname, dto.TelephoneNumber, dto.Icon, dto.DateOfBirth)
+	defer rows.Close()
+	return err
 }
 
 func (r employeeAccountRepository) MoveEmployeeToDepartment(ctx context.Context, employeeId int, departmentFromId int, departmentToId int) error {
-	//TODO implement me
-	panic("implement me")
+	query := `
+		UPDATE employee_department
+		SET departmentid=$3
+		WHERE accountid=$1 AND departmentid=$2
+	`
+	rows, err := r.conn.Query(ctx, query, employeeId, departmentFromId, departmentToId)
+	defer rows.Close()
+	return err
 }
 
 func (r employeeAccountRepository) AddEmployeeToDepartment(ctx context.Context, employeeId int, departmentId int) error {
-	//TODO implement me
-	panic("implement me")
+	query := `
+		INSERT INTO employee_department
+		(accountid, departmentid)
+		VALUES ($1, $2)
+	`
+	rows, err := r.conn.Query(ctx, query, employeeId, departmentId)
+	defer rows.Close()
+	return err
 }
