734404d703ba76570f195eae06375b96c83d672a
Алексей Замятин <79986929+RoboutGulman@users.noreply.github.com>
2023-12-20 16:54:03 +0300

    заменил Wrap на WrapWithBody в создании департамента

--- company/impl/infrastructure/transport/frontend.go
+++ company/impl/infrastructure/transport/frontend.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"errors"
 	openapi_types "github.com/oapi-codegen/runtime/types"
-	"io/ioutil"
 	"net/http"
 	authInteralapi "portal_back/authentication/api/internalapi"
 	"portal_back/company/api/frontend"
@@ -52,20 +51,8 @@ func (f frontendServer) GetDepartments(w http.ResponseWriter, r *http.Request) {
 }
 
 func (f frontendServer) CreateDepartment(w http.ResponseWriter, r *http.Request) {
-	network.Wrap(f.authRequestService, w, r, func(info network.RequestInfo) {
-		reqBody, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		var departmentReq frontendapi.DepartmentRequest
-		err = json.Unmarshal(reqBody, &departmentReq)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		err = f.departmentService.CreateDepartment(r.Context(), mapper.MapDepartmentRequest(departmentReq), info.CompanyId)
+	network.WrapWithBody(f.authRequestService, w, r, func(info network.RequestInfo, request frontendapi.DepartmentRequest) {
+		err := f.departmentService.CreateDepartment(r.Context(), mapper.MapDepartmentRequest(request), info.CompanyId)
 
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
